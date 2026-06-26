f862bead7c4d6a7d863cacb37c97f2a84605a67d
Gannoji Durga Prasad <gannoji16@gmail.com>
2026-06-26 23:11:03 +0530

    adding xam proctor image

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -433,7 +433,7 @@ Featured Projects
     className="bg-gray-900/70 backdrop-blur-lg border border-gray-800 rounded-2xl overflow-hidden cursor-pointer"
   >
     <img
-      src="\public\proctoring-system.png"
+      src="\proctoring-system.png"
       alt="Exam Proctoring System"
       className="w-full h-52 object-cover"
     />
